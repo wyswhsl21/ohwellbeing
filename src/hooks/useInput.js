819a35ed35cce1ce6531ccb5a5710154487c84ec
sooyun <so__oyeonii@naver.com>
2022-10-16 15:27:29 +0900

    컴포넌트 함수형으로 수정 / useInput으로 customhook / link1개 삽입

---
+++ src/hooks/useInput.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const useInput = () => {
+  const [dadat, setDadat] = useState({
+    nickname: "",
+    memo: "",
+  });
+
+  const dadatChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setDadat({ ...dadat, [name]: value });
+  };
+  return [dadat, dadatChangeHandler];
+};
+
+export default useInput;
