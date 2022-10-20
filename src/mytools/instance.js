7f66fea748e08316f3693015a3232d78ef727f7f
wyswhsl21 <wyswhsl21@naver.com>
2022-10-20 15:32:33 +0900

    헤로쿠#1

--- src/mytools/instance.js
+++ src/mytools/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.REACT_APP_URL,
 });
 
 //ohwell instance Api 추가# 사용법 ex)  const data = await ohwellApi.getTodo();
