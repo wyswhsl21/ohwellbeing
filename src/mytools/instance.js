02d4b980570f651765ded5d09c771105e449abdf
sooyun <so__oyeonii@naver.com>
2022-10-20 20:08:18 +0900

    no message

--- src/mytools/instance.js
+++ src/mytools/instance.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 
 const instance = axios.create({
@@ -23,29 +22,3 @@ export const dadatApi = {
   patchDadat: (dadatId, edit) =>
     instance.patch(`/dadats/${dadatId}`, { memo: edit }),
 };
-=======
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: process.env.REACT_APP_URL,
-});
-
-//ohwell instance Api 추가# 사용법 ex)  const data = await ohwellApi.getTodo();
-//사용 하다보면 익숙해질거에요 *^^*
-export const ohwellApi = {
-  getOhwell: () => instance.get("/ohwell"), //각각의 get,post,delete,patch입니다!
-  getOhwells: (arg) => instance.get(`/ohwell/${arg}`), //각각의 get,post,delete,patch입니다!
-  postOhwell: (ohwell) => instance.post("/ohwell", ohwell),
-  deleteOhwell: (ohwellId) => instance.delete(`/ohwell/${ohwellId}`),
-  patchOhwell: (ohwellId, edit) =>
-    instance.patch(`/ohwell/${ohwellId}`, { memo: edit }),
-};
-
-export const dadatApi = {
-  getDadat: () => instance.get("/dadats"),
-  postDadat: (dadat) => instance.post("/dadats", dadat),
-  deleteDadat: (dadatId) => instance.delete(`/dadats/${dadatId}`),
-  patchDadat: (dadatId, edit) =>
-    instance.patch(`/dadats/${dadatId}`, { memo: edit }),
-};
->>>>>>> master
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
@@ -19,25 +18,3 @@ const store = configureStore({
 });
 
 export default store;
-=======
-import { createStore } from "redux";
-import { configureStore } from "@reduxjs/toolkit";
-
-/*reducer toolkit Slice.reducer
- */
-import ohsik from "../modules/ohsikSlice";
-import dadat from "../modules/dadatSlice";
-import ohsiks from "../modules/ohsiksSlice";
-
-// 내가 만든 리듀서를 넣어준다
-const store = configureStore({
-  reducer: {
-    ohsik: ohsik,
-    ohsiks: ohsiks,
-    dadat: dadat,
-  },
-  devTools: process.env.NODE_ENV !== "production",
-});
-
-export default store;
->>>>>>> master
